909fe497e9128a8385e8cc4f2b08dd31bdd7f9e7
Jochen Scheib <mail@jochen-scheib.de>
2016-04-18 20:17:58 +0200

    Bugfix: Use Array.isArray instead of instanceof Array

--- nodes/ui_chart.js
+++ nodes/ui_chart.js
@@ -4,15 +4,15 @@ module.exports = function(RED) {
     function ChartNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        
+
         var tab = RED.nodes.getNode(config.tab);
         if (!tab) return;
-        
+
         var options = {
             emitOnlyNewValues: false,
-            node: node, 
-            tab: tab, 
-            group: config.group, 
+            node: node,
+            tab: tab,
+            group: config.group,
             control: {
                 type: 'chart',
                 order: config.order,
@@ -20,14 +20,14 @@ module.exports = function(RED) {
                 nodata: config.nodata
             },
             convert: function(value, oldValue, msg) {
-                if (value instanceof Array) {
+                if (Array.isArray(value)) {
                     oldValue = value;
                 } else {
                     value = parseFloat(value);
                     if (isNaN(value)) return;
                     var topic = msg.topic || 'Data';
-                    if (!oldValue) oldValue = []; 
-                    
+                    if (!oldValue) oldValue = [];
+
                     var found;
                     for (var i=0; i<oldValue.length; i++) {
                         if (oldValue[i].key === topic) {
@@ -39,34 +39,34 @@ module.exports = function(RED) {
                         found = { key: topic, values: [] };
                         oldValue.push(found);
                     }
-                    
+
                     var time = new Date().getTime();
                     var point = [time, value];
                     found.values.push(point);
-                    
+
                     var limitOffsetSec = parseInt(config.removeOlder) * parseInt(config.removeOlderUnit);
                     var limitTime = new Date().getTime() - limitOffsetSec * 1000;
-                    
+
                     var remove = [];
                     oldValue.forEach(function (series, index) {
                         var i=0;
                         while (i<series.values.length && series.values[i][0]<limitTime) i++;
                         if (i) series.values.splice(0, i);
-                        
+
                         if (series.values.length === 0)
                             remove.push(index);
                     });
-                    
+
                     remove.forEach(function (index) {
                         oldValue.splice(index, 1);
                     });
                 }
-                
+
                 return oldValue;
             }
         };
         var done = ui.add(options);
-        
+
         setTimeout(function() {node.send([null, {payload: "restore", for: node.id}]);}, 100);
 
         node.on("close", done);
